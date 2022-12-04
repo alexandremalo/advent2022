ccd2f1be446df2a5d8eb6877a16129c447d2ca59
Alexandre Malo <alexandre.malo@bell.ca>
2022-12-04 11:43:40 -0500

    day 4

---
+++ day4/solver2.py
@@ -0,0 +1,33 @@
+def array_contains_other(start_one, end_one, start_two, end_two):
+    to_return = False
+    if start_one <= start_two <= end_one:
+        to_return = True
+    elif start_two <= start_one <= end_two:
+        to_return = True
+    return to_return
+
+
+
+def solve():
+    with open('input/input1', 'r') as file:
+        total_full_duplicate_work = 0
+        for line in file:
+            pair_of_assignment = line.strip().split(',')
+            elf_one_assignment = pair_of_assignment[0].split('-')
+            elf_one_start_index = int(elf_one_assignment[0])
+            elf_one_end_index = int(elf_one_assignment[1])
+            elf_two_assignment = pair_of_assignment[1].split('-')
+            elf_two_start_index = int(elf_two_assignment[0])
+            elf_two_end_index = int(elf_two_assignment[1])
+            if array_contains_other(elf_one_start_index, elf_one_end_index, elf_two_start_index, elf_two_end_index):
+                total_full_duplicate_work += 1
+        print("=== Result ===")
+        print(total_full_duplicate_work)
+        #print(elf_one_start_index)
+        #print(elf_one_end_index)
+        #print(elf_two_start_index)
+        #print(elf_two_end_index)
+        print("==============")
+
+if __name__ == '__main__':
+    solve()
