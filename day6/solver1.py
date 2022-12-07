bd773d4bbd954ce95762a25479b91e06e9f8ce82
Alexandre Malo <alexandre.malo@bell.ca>
2022-12-06 19:41:22 -0500

    day6

---
+++ day6/solver1.py
@@ -0,0 +1,30 @@
+def first_four_diff_char_position(radio_signal):
+    char_index = 3
+    found_four_diff = False
+    while found_four_diff == False or char_index <= len(radio_signal) - 1:
+        found_duplicated = False
+        char_set = {}
+        for x in range(4):
+            #print(radio_signal[char_index - x])
+            if char_set.get(radio_signal[char_index - x], 0) != 0:
+                found_duplicated = True
+            char_set[radio_signal[char_index - x]] = 1
+        char_index += 1
+        if found_duplicated == False:
+            print(char_index)
+            break
+    return char_index
+
+
+def solve():
+    with open('input/input1', 'r') as file:
+        for line in file:
+            radio_signal_position = first_four_diff_char_position(line)
+        print("=== Result ===")
+        print(len(line))
+        print(radio_signal_position)
+        print("==============")
+
+
+if __name__ == '__main__':
+    solve()
